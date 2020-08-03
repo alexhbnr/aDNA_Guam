555c838ef6025c152a1d3f779337cefff3170f00
Alex Huebner <alexhbnr@gmail.com>
2020-08-03 14:16:12 +0200

    Update mapDamage analysis and generate summary and detail plots

--- 07-publication/scripts/QUAL_mapDamage_plt-plot_damage_shotgun.R
+++ 07-publication/scripts/QUAL_mapDamage_plt-plot_damage_shotgun.R
@@ -2,36 +2,25 @@ library(data.table)
 library(tidyverse)
 library(patchwork)
 
-# Load shotgun runs
-samples <- c("SP4210", "SP4211")
-libdirs <- map(samples, function(s) list.files(path = paste0(snakemake@params[["dir"]], s),
-                                               pattern = ".+-160818_SN7001204_0542_lane2$",
-                                               full.names = T)) %>%
-           unlist()
+load(snakemake@input[[1]])
+shotgun_runs <- fread(snakemake@params[["array"]]) %>%
+                filter(array == "shotgun") %>%
+                pull(seqrun_id)
 
-misincorporation <- map_dfr(libdirs, function(fn) {
-                      fread(paste(fn, "misincorporation.txt", sep = "/"),
-                            select = c(1:8, 10:21)) %>%
-                      pivot_longer(`G>A`:`G>T`, names_to = "subst", values_to = "count") %>%
-                      pivot_longer(A:T, names_to = "refbase", values_to = "refcount") %>%
-                      filter(str_sub(subst, 1, 1) == refbase) %>%
-                      group_by(End, Pos, subst) %>%
-                      summarise(frac = sum(count) / sum(refcount)) %>%
-                      filter(Pos <= 15) %>%
-                      mutate(sample = str_match(basename(fn), "(SP[0-9]+)-[A-Z][0-9]+-[0-9]+_.+")[,2])
-                    }) %>%
-                    select(sample, end = End, pos = Pos, subst, frac) %>%
-                    mutate(plot_pos = ifelse(end == "3p", pos * (-1), pos),
-                           end = factor(ifelse(end == "3p", "3'", "5'"), levels = c("5'", "3'"))) %>%
-                    left_join(tibble(subst = c("C>T", "G>A", "A>C", "A>G", "A>T", "C>A", "C>G", "G>C", "G>T", "T>A", "T>C", "T>G"),
-                                     col_cat = c("C>T", "G>A", rep("other", 10))), by = "subst") %>%
-                    mutate(sample = recode(sample, SP4210 = "RBC1",
-                                                   SP4211 = "RBC2"))
+# Subset shotgun runs
+shotgun <- filter(misincorporation, seqrun %in% shotgun_runs) %>%
+           mutate(sample = recode(sample, SP4210 = "RBC1",
+                                          SP4211 = "RBC2"),
+                  plot_pos = ifelse(end == "3p", pos * (-1), pos),
+                  end = factor(ifelse(end == "3p", "3'", "5'"), levels = c("5'", "3'")),
+                  lib = str_c(sample, " - ", lib)) %>%
+           left_join(tibble(subst = c("C>T", "G>A", "A>C", "A>G", "A>T", "C>A", "C>G", "G>C", "G>T", "T>A", "T>C", "T>G"),
+                            col_cat = c("C>T", "G>A", rep("other", 10))), by = "subst")
 
-plot_damage <- function(misinc, smp, xaxis_title = T) {
-  p <- filter(misinc, sample == smp) %>%
-       ggplot(aes(x = plot_pos, group = subst)) +
-       geom_line(aes(colour = col_cat, y = frac)) +
+plot_damage_summary <- function(misinc, smp, xaxis_title = T) {
+  p <- filter(misinc, lib == smp) %>%
+       ggplot(aes(x = plot_pos, y = frac, colour = col_cat, group = subst)) +
+       geom_line() +
        facet_wrap(~ end, ncol = 2, scales = "free_x") +
        labs(x = "position at read end",
             y = "frequency",
@@ -42,19 +31,21 @@ plot_damage <- function(misinc, smp, xaxis_title = T) {
        scale_y_continuous(labels = scales::percent_format(accuracy = 1),
                           limits = c(0, 0.5)) +
        scale_color_manual(values = c("red", "blue", "grey50")) +
-       theme_classic(base_size = 10) +
-       theme(legend.position = "top",
-             plot.title = element_text(hjust = 0.5, face = "bold", size = 9),
-             axis.text.x = element_text(size = 6),
+       theme_classic(base_size = 8) +
+       theme(legend.position = "right",
+             plot.title = element_text(hjust = 0.5, face = "bold", size = 8),
+             axis.text.x = element_text(size = 4),
+             axis.text.y = element_text(size = 6),
              strip.background.x = element_blank())
   if (!xaxis_title) p + theme(axis.title.x = element_blank())
   else p
 }
 
-smiley_plt <- plot_damage(misincorporation, "RBC1", xaxis_title = F) +
-              plot_damage(misincorporation, "RBC2", xaxis_title = T) +
-              guide_area() +
-              plot_layout(ncol = 1, heights = c(1, 1, 0.2), guides = "collect")
+damage_plots <- vector(mode = "list", length = length(unique(shotgun$lib)))
+for (i in 1:length(unique(shotgun$lib))) {
+    damage_plots[[i]] <- plot_damage_summary(shotgun, unique(shotgun$lib)[[i]], xaxis_title = T)
+}
+plt <- wrap_plots(damage_plots, ncol = 4, guides = "collect")
 
-ggsave(snakemake@output[[1]], plot = smiley_plt,
-       width = 160, height = 100, units = "mm", useDingbats = F)
+ggsave(snakemake@output[[1]], plot = plt,
+       width = 250, height = 180, units = "mm", useDingbats = F)
--- 07-publication/scripts/QUAL_mapDamage_plt-plot_damage_summary.R
+++ 07-publication/scripts/QUAL_mapDamage_plt-plot_damage_summary.R
@@ -3,49 +3,53 @@ library(patchwork)
 
 # Load data
 load(snakemake@input[[1]])
+misincorporation <- mutate(misincorporation, sample = recode(sample, SP4210 = "RBC1",
+                                                                     SP4211 = "RBC2"))
 
-plot_damage <- function(misinc, smp, legend = "none") {
-  misinc %>%
-  filter(sample == smp) %>%
-  # Summarise across libraries and calculate 95% CI
-  group_by(sample, end, pos, subst) %>%
-  summarise(meanFrac = mean(frac),
-            sdFrac = sd(frac),
-            seFrac = qnorm(.975) * sdFrac / sqrt(length(frac))) %>%
-  ungroup() %>%
-  # Annotate for plotting 
-  left_join(tibble(subst = c("C>T", "G>A", "A>C", "A>G", "A>T", "C>A", "C>G", "G>C", "T>A", "T>C", "T>G"),
-                   col_cat = c("C>T", "G>A", rep("other", 9))), by = "subst") %>%
-  mutate(plot_pos = ifelse(end == "3p", pos * (-1), pos),
-         end = factor(ifelse(end == "3p", "3'", "5'"), levels = c("5'", "3'")),
-         seFrac = ifelse(col_cat == "other", 0, seFrac)) %>%
-  # Plot
-  ggplot(aes(x = plot_pos, group = subst)) +
-  geom_line(aes(colour = col_cat, y = meanFrac)) +
-  geom_ribbon(aes(ymin = meanFrac - seFrac,
-                  ymax = meanFrac + seFrac,
-                  fill = col_cat),
-              alpha = 0.5) +
-  facet_wrap(~ end, ncol = 2, scales = "free_x") +
-  labs(x = "position at read end",
-       y = "mean fraction",
-       colour = "substitution type",
-       fill = "substitution type",
-       title = smp) +
-  scale_x_continuous(breaks = seq(-15, 15, 1)) +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1),
-                     limits = c(0, 0.5)) +
-  scale_color_manual(values = c("red", "blue", "grey50")) +
-  scale_fill_manual(values = c("red", "blue", "grey50")) +
-  theme_classic(base_size = 10) +
-  theme(legend.position = "top",
-        plot.title = element_text(hjust = 0.5, face = "bold", size = 9),
-        axis.text.x = element_text(size = 6),
-        strip.background.x = element_blank())
+plot_damage <- function(misinc, smp, xaxis_title = T) {
+  p <- misinc %>%
+       filter(sample == smp) %>%
+       # Summarise across libraries and calculate 95% CI
+       group_by(sample, end, pos, subst) %>%
+       summarise(meanFrac = mean(frac),
+                 sdFrac = sd(frac),
+                 seFrac = qnorm(.975) * sdFrac / sqrt(length(frac))) %>%
+       ungroup() %>%
+       # Annotate for plotting 
+       left_join(tibble(subst = c("C>T", "G>A", "A>C", "A>G", "A>T", "C>A", "C>G", "G>C", "T>A", "T>C", "T>G"),
+                        col_cat = c("C>T", "G>A", rep("other", 9))), by = "subst") %>%
+       mutate(plot_pos = ifelse(end == "3p", pos * (-1), pos),
+              end = factor(ifelse(end == "3p", "3'", "5'"), levels = c("5'", "3'")),
+              seFrac = ifelse(col_cat == "other", 0, seFrac)) %>%
+       # Plot
+       ggplot(aes(x = plot_pos, group = subst)) +
+       geom_line(aes(colour = col_cat, y = meanFrac)) +
+       geom_ribbon(aes(ymin = meanFrac - seFrac,
+                       ymax = meanFrac + seFrac,
+                       fill = col_cat),
+                   alpha = 0.5) +
+       facet_wrap(~ end, ncol = 2, scales = "free_x") +
+       labs(x = "position at read end",
+            y = "mean fraction",
+            colour = "substitution type",
+            fill = "substitution type",
+            title = smp) +
+       scale_x_continuous(breaks = seq(-15, 15, 1)) +
+       scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+                          limits = c(0, 0.5)) +
+       scale_color_manual(values = c("red", "blue", "grey50")) +
+       scale_fill_manual(values = c("red", "blue", "grey50")) +
+       theme_classic(base_size = 10) +
+       theme(legend.position = "top",
+             plot.title = element_text(hjust = 0.5, face = "bold", size = 9),
+             axis.text.x = element_text(size = 6),
+             strip.background.x = element_blank())
+  if (!xaxis_title) p + theme(axis.title.x = element_blank())
+  else p
 }
 
-smiley_plt <- plot_damage(misincoporation, "SP4210") +
-              plot_damage(misincoporation, "SP4211", legend = "right") +
+smiley_plt <- plot_damage(misincorporation, "RBC1", xaxis_title = F) +
+              plot_damage(misincorporation, "RBC2", xaxis_title = T) +
               guide_area() +
               plot_layout(ncol = 1, heights = c(1, 1, 0.2), guides = "collect")
 
